05eb9044168f27e8c679effc4e6a6fc5c3d142d1
isaias.neto <isaias.neto@ifood.com.br>
2019-09-11 20:04:35 -0300

    commit initial project

---
+++ src/main/java/br/com/ufg/listaplic/ListAplicApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufg.listaplic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListAplicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListAplicApplication.class, args);
+	}
+
+}
