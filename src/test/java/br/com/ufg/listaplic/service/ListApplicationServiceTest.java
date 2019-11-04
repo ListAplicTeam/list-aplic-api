3f519c4e6be798a165a73d22a66a0dabc24e975e
João Pedro Pinheiro <jprnp18@gmail.com>
2019-11-04 11:12:32 -0300

    CDU17 - Testes Unitários 2

--- src/test/java/br/com/ufg/listaplic/service/ListApplicationServiceTest.java
+++ src/test/java/br/com/ufg/listaplic/service/ListApplicationServiceTest.java
@@ -3,13 +3,12 @@ package br.com.ufg.listaplic.service;
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.ufg.listaplic.base.BaseTest;
 import br.com.ufg.listaplic.dto.ListApplicationDTO;
-import br.com.ufg.listaplic.model.ApplicationListStatus;
-import br.com.ufg.listaplic.model.Classroom;
-import br.com.ufg.listaplic.model.ListApplication;
-import br.com.ufg.listaplic.model.Student;
+import br.com.ufg.listaplic.model.*;
+import br.com.ufg.listaplic.repository.AnswerJpaRepository;
 import br.com.ufg.listaplic.repository.ClassroomJpaRepository;
 import br.com.ufg.listaplic.repository.ListApplicationJpaRepository;
 import br.com.ufg.listaplic.repository.StudentJpaRepository;
+import br.com.ufg.listaplic.template.AnswerTemplate;
 import br.com.ufg.listaplic.template.ClassroomTemplate;
 import br.com.ufg.listaplic.template.ListApplicationTemplate;
 import br.com.ufg.listaplic.template.StudentTemplate;
@@ -36,6 +35,8 @@ public class ListApplicationServiceTest extends BaseTest {
     private ClassroomJpaRepository mockClassroomJpaRepository;
     @Mock
     private StudentJpaRepository mockStudentJpaRepository;
+    @Mock
+    private AnswerJpaRepository mockAnswerJpaRepository;
 
     @Test
     public void testGetFinishedListsByClassroomId() {
@@ -52,5 +53,25 @@ public class ListApplicationServiceTest extends BaseTest {
 
         // Verify the results
         assertEquals(applications.size(), result.size());
+    }
+
+    @Test
+    public void testGetListApplicationDetail() {
+        // Setup
+        ListApplication application = Fixture.from(ListApplication.class).gimme(ListApplicationTemplate.TYPES.FINISHED_APPLICATION.name());
+        List<Answer> answers = Fixture.from(Answer.class).gimme(2, AnswerTemplate.TYPES.ANSWER.name());
+        List<Student> students = Fixture.from(Student.class).gimme(1, StudentTemplate.TYPES.STUDENT_WITH_ID.name());
+        when(mockListApplicationJpaRepository.findById(any(UUID.class))).thenReturn(java.util.Optional.of(application));
+        when(mockAnswerJpaRepository.findByApplicationId(any(UUID.class))).thenReturn(answers);
+        when(mockStudentJpaRepository.findStudentsByClassroomId(any(UUID.class))).thenReturn(students);
+
+        // Run the Test
+        final ListApplicationDTO result = listApplicationServiceUnderTest.getListApplicationDetail(application.getId());
+
+        // Verify the result
+        assertEquals(application.getId(), result.getId());
+        assertEquals(application.getList(), result.getListId());
+        assertEquals(application.getClassroom().getId(), result.getGroupId());
+        assertEquals(answers.size(), result.getAnswerList().size());
     }
 }
