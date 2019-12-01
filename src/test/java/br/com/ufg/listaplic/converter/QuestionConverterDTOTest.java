99392e7e793e79ca3a89b2d23b6b7107ad892020
isaias.neto <isaias.neto@ifood.com.br>
2019-12-01 11:26:35 -0300

    improve unit test

--- src/test/java/br/com/ufg/listaplic/converter/QuestionConverterDTOTest.java
+++ src/test/java/br/com/ufg/listaplic/converter/QuestionConverterDTOTest.java
@@ -29,4 +29,73 @@ public class QuestionConverterDTOTest extends BaseTest {
         assertEquals(questaoIntegrationDTO.getRespostaEsperada().size(), result.getExpectedAnswers().size());
     }
 
+    @Test
+    public void testFromQuestionIntegrationToQuestionDTOWhenExpectedAnswerIsNull() {
+        // Setup
+        final QuestaoIntegrationDTO questaoIntegrationDTO = Fixture.from(QuestaoIntegrationDTO.class).gimme(QuestaoIntegrationDTOTemplate.TYPES.DISCURSIVE.name());
+        questaoIntegrationDTO.setRespostaEsperada(null);
+
+        // Run the test
+        final QuestionDTO result = QuestionConverterDTO.fromDomainToDTO(questaoIntegrationDTO);
+
+        // Verify the results
+        assertEquals(questaoIntegrationDTO.getId(), result.getId());
+        assertEquals(questaoIntegrationDTO.getEnunciado(), result.getName());
+        assertEquals(QuestionType.DISCURSIVE, result.getType());
+        assertEquals(null, result.getOptions());
+        assertEquals(null, result.getAnswer());
+        assertEquals(null, result.getExpectedAnswers());
+    }
+
+    @Test
+    public void testFromQuestionIntegrationToQuestionDTOWhenExpectedAnswerIsMultipleChoice() {
+        // Setup
+        final QuestaoIntegrationDTO questaoIntegrationDTO = Fixture.from(QuestaoIntegrationDTO.class).gimme(QuestaoIntegrationDTOTemplate.TYPES.MULTIPLE_CHOICE.name());
+
+        // Run the test
+        final QuestionDTO result = QuestionConverterDTO.fromDomainToDTO(questaoIntegrationDTO);
+
+        // Verify the results
+        assertEquals(questaoIntegrationDTO.getId(), result.getId());
+        assertEquals(questaoIntegrationDTO.getEnunciado(), result.getName());
+        assertEquals(QuestionType.MULTIPLE_CHOICE, result.getType());
+        assertEquals(null, result.getOptions());
+        assertEquals(null, result.getAnswer());
+        assertEquals(questaoIntegrationDTO.getRespostaEsperada().size(), result.getExpectedAnswers().size());
+    }
+
+    @Test
+    public void testFromQuestionIntegrationToQuestionDTOWhenExpectedAnswerIsColumnBinding() {
+        // Setup
+        final QuestaoIntegrationDTO questaoIntegrationDTO = Fixture.from(QuestaoIntegrationDTO.class).gimme(QuestaoIntegrationDTOTemplate.TYPES.COLUMN_BINDING.name());
+
+        // Run the test
+        final QuestionDTO result = QuestionConverterDTO.fromDomainToDTO(questaoIntegrationDTO);
+
+        // Verify the results
+        assertEquals(questaoIntegrationDTO.getId(), result.getId());
+        assertEquals(questaoIntegrationDTO.getEnunciado(), result.getName());
+        assertEquals(QuestionType.COLUMN_BINDING, result.getType());
+        assertEquals(null, result.getOptions());
+        assertEquals(null, result.getAnswer());
+        assertEquals(questaoIntegrationDTO.getRespostaEsperada().size(), result.getExpectedAnswers().size());
+    }
+
+    @Test
+    public void testFromQuestionIntegrationToQuestionDTOWhenExpectedAnswerIsTrueOrFalse() {
+        // Setup
+        final QuestaoIntegrationDTO questaoIntegrationDTO = Fixture.from(QuestaoIntegrationDTO.class).gimme(QuestaoIntegrationDTOTemplate.TYPES.TRUE_OR_FALSE.name());
+
+        // Run the test
+        final QuestionDTO result = QuestionConverterDTO.fromDomainToDTO(questaoIntegrationDTO);
+
+        // Verify the results
+        assertEquals(questaoIntegrationDTO.getId(), result.getId());
+        assertEquals(questaoIntegrationDTO.getEnunciado(), result.getName());
+        assertEquals(QuestionType.TRUE_OR_FALSE, result.getType());
+        assertEquals(null, result.getOptions());
+        assertEquals(null, result.getAnswer());
+        assertEquals(questaoIntegrationDTO.getRespostaEsperada().size(), result.getExpectedAnswers().size());
+    }
+
 }
